86cf8cbfaf50274c5c42a6fadb274c53c001d9b8
xichen <xichen888.private@gmail.com>
2023-10-20 13:11:56 +0800

    init project

---
+++ src/main/java/com/example/mypetstoreserver/MyPetStoreServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.mypetstoreserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPetStoreServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyPetStoreServerApplication.class, args);
+    }
+
+}
